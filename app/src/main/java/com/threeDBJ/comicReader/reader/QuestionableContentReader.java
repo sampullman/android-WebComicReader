8a1cbe41bc2e170afb6c14b58ecd9eca37b31d9c
Sam Pullman <sampullman@gmail.com>
2018-10-05 01:47:54 -0700

    Fix questionable content

--- app/src/main/java/com/threeDBJ/comicReader/reader/QuestionableContentReader.java
+++ app/src/main/java/com/threeDBJ/comicReader/reader/QuestionableContentReader.java
@@ -10,12 +10,10 @@ import java.util.regex.Pattern;
 import timber.log.Timber;
 
 public class QuestionableContentReader extends Reader {
-    public static String prevPat = "<a href=\"view.php[?]comic=([0-9]+)\">Previous</a>";
-    public static String nextPat = "<a href=\"view.php[?]comic=([0-9]+)\">Next</a>";
-    public static String imgPat = "<img id=\"strip\" src=.*?/comics/([0-9]+)([.][a-zA-Z]+?)\">";
-    public static String comicBase = "http://questionablecontent.net/view.php?comic=";
-    public static String comicMax = "http://questionablecontent.net/index.php";
-    public static String comicFirstInd = "1";
+
+    public static String prevPat = "<li><a href=\"view.php[?]comic=([0-9]+)\">Previous</a></li>";
+    public static String nextPat = "<li><a href=\"view.php[?]comic=([0-9]+)\">Next</a></li>";
+    public static String imgPat = "<img.*?src=\".*?/comics/([0-9]+)([.][a-zA-Z]+?)\">";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,25 +24,18 @@ public class QuestionableContentReader extends Reader {
 
     @Override
     public void UISetup() {
+        super.UISetup();
         this.pPrev = Pattern.compile(prevPat, Pattern.DOTALL | Pattern.UNIX_LINES);
         this.pNext = Pattern.compile(nextPat, Pattern.DOTALL | Pattern.UNIX_LINES);
         this.pImages = Pattern.compile(imgPat, Pattern.DOTALL | Pattern.UNIX_LINES);
 
+        this.base = "http://questionablecontent.net/view.php?comic=";
+        this.max = "http://questionablecontent.net/index.php";
+        this.firstInd = "1";
+
         this.title = "Questionable Content";
         this.shortTitle = "Questionable";
         this.storeUrl = "http://www.topatoco.com/merchant.mvc?Screen=CTGY&Store_Code=TO&Category_Code=QC";
-    }
-
-    public String getBase() {
-        return comicBase;
-    }
-
-    public String getMax() {
-        return comicMax;
-    }
-
-    public String getFirstInd() {
-        return comicFirstInd;
     }
 
     public String handleRawPage(Comic c, String page) {
@@ -53,23 +44,27 @@ public class QuestionableContentReader extends Reader {
         Matcher nextMatcher = pNext.matcher(page);
 
         boolean success = imageMatcher.find();
-        String imgUrl = "http://questionablecontent.net/comics/" + imageMatcher.group(1) + imageMatcher.group(2);
-        if(prevMatcher.find()) {
-            Timber.d("prev: %s", prevMatcher.group(1));
-            c.setPrevInd(prevMatcher.group(1));
-            if(nextMatcher.find() && nextMatcher.group(1) != null) {
-                c.setNextInd(nextMatcher.group(1));
+        if(success) {
+            String imgUrl = "https://questionablecontent.net/comics/" + imageMatcher.group(1) + imageMatcher.group(2);
+            Timber.e("Success %s", imgUrl);
+            if (prevMatcher.find()) {
+                Timber.d("prev: %s", prevMatcher.group(1));
+                c.setPrevInd(prevMatcher.group(1));
+                if (nextMatcher.find() && nextMatcher.group(1) != null) {
+                    c.setNextInd(nextMatcher.group(1));
+                } else {
+                    setMaxIndex(imageMatcher.group(1));
+                    setMaxNum(imageMatcher.group(1));
+                }
             } else {
-                setMaxIndex(imageMatcher.group(1));
-                setMaxNum(imageMatcher.group(1));
+                if (nextMatcher.find(1)) {
+                    c.setNextInd(nextMatcher.group(1));
+                } else {
+                    /* Anomaly, this should not happen */
+                }
             }
-        } else {
-            if(nextMatcher.find(1)) {
-                c.setNextInd(nextMatcher.group(1));
-            } else {
-                /* Anomaly, this should not happen */
-            }
+            return imgUrl;
         }
-        return imgUrl;
+        return null;
     }
 }
