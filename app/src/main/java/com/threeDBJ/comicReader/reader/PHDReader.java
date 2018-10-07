fead79e0384fc1d307f5803be2476531bfa58067
Sam Pullman <sampullman@gmail.com>
2018-10-07 00:54:11 -0700

    Fix PHD comics

--- app/src/main/java/com/threeDBJ/comicReader/reader/PHDReader.java
+++ app/src/main/java/com/threeDBJ/comicReader/reader/PHDReader.java
@@ -7,6 +7,8 @@ import com.threeDBJ.comicReader.Comic;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import timber.log.Timber;
+
 public class PHDReader extends Reader {
 
     Pattern pImages, pPrev, pNext;
@@ -22,8 +24,8 @@ public class PHDReader extends Reader {
     public void UISetup() {
         super.UISetup();
 
-        String prevPat = "archive.php\\?comicid=([0-9]+).*?prev_button.gif";
-        String nextPat = "<td align=\"left\" valign=\"top\"><a href=archive.php\\?comicid=([0-9]+).*?next_button.gif";
+        String prevPat = "<td align=\"left\">.*?archive.php\\?comicid=([0-9]+).*?Previous Comic";
+        String nextPat = "<td align=\"right\">.*?archive.php\\?comicid=([0-9]+).*?Next Comic";
         String imgPat = "src=(http://www.phdcomics.com/comics/archive/(.*?)\\.gif)";
 
         this.pImages = Pattern.compile(imgPat, Pattern.DOTALL | Pattern.UNIX_LINES);
@@ -43,13 +45,17 @@ public class PHDReader extends Reader {
         Matcher prevMatcher = pPrev.matcher(page);
         Matcher nextMatcher = pNext.matcher(page);
 
+        Timber.e("AWWW");
         imageMatcher.find();
         String imgUrl = imageMatcher.group(1);
+        Timber.e("HECK");
 
         if(prevMatcher.find()) {
             c.setPrevInd(prevMatcher.group(1));
+            Timber.e("prev %s %s", prevMatcher.group(1), imgUrl);
             /* Normal comic */
             if(nextMatcher.find()) {
+                Timber.e("Next: %s", nextMatcher.group(1));
                 c.setNextInd(nextMatcher.group(1));
                 /* Last comic */
             } else {
@@ -57,8 +63,9 @@ public class PHDReader extends Reader {
                 setMaxIndex(temp);
                 setMaxNum(temp);
             }
-            /* First comic */
         } else {
+            /* First comic */
+            Timber.e("No prev (first)");
             if(nextMatcher.find(1)) {
                 c.setNextInd(nextMatcher.group(1));
             } else {
